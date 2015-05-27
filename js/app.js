ffae9edd5d348189467b1ad2b640f6e56a2eb959
Paul Joannon <hello@pauljoannon.com>
2015-05-27 10:06:10 +0200

    use imagesloaded

--- js/app.js
+++ js/app.js
@@ -56,7 +56,9 @@ app.controller('Ctrl', ['$scope', '$timeout', '$http', function($scope, $timeout
                 }
 
                 $scope.$apply(function() { // Make sure we're in an applied scope
-                    msnry.layout(); // Re-layout
+                    imagesLoaded($('.masonry')[0], function() {
+                        msnry.layout(); // Re-layout
+                    });
                 });
             });
 
@@ -67,10 +69,14 @@ app.controller('Ctrl', ['$scope', '$timeout', '$http', function($scope, $timeout
 
     var $win = $(window);
     $(document).on('scroll', _.debounce(function() {
-        if ($win.height() + $win.scrollTop() === $(document).height()) {
+        var scrollTop = $win.scrollTop()
+        if ($win.height() + scrollTop === $(document).height()) {
             $scope.$apply(function() {
                 $scope.data = $scope.data.concat(allData.splice(0, 100));
             });
+            $timeout(function() {
+                $win.scrollTop(scrollTop);
+            }, 400);
         }
     }));
 }]);
